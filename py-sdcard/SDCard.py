f018c348cc1820e90ae48b15bfcf5bc0c43c569b
aushacker <25114336+aushacker@users.noreply.github.com>
2024-05-09 23:54:41 +1000

    SD card initializing

--- py-sdcard/SDCard.py
+++ py-sdcard/SDCard.py
@@ -32,7 +32,7 @@ class Command:
         elif self._index == 8:
             result.append(0x87)
         else:
-            result.append(0)
+            result.append(1)
 
         return result
 
@@ -52,20 +52,31 @@ class Response:
         x = None if self.extra == None else f'{self.extra:#x}'
         return f'Response(0b{self.response:08b},{x})'
 
+    def is_illegal_command(self) -> bool:
+        return (self.response & 0b100) != 0
+
 class SDCard:
     """A layer above the SPI hardware, manages the SD card state and data exchange."""
 
     commands = {
         'CMD0': Command('CMD0', 0),
-        'CMD8': Command('CMD8', 8, 7)
+        'CMD8': Command('CMD8', 8, 7),
+        'CMD55': Command('CMD55', 55),
+        'CMD58': Command('CMD58', 58, 3),
+        'ACMD41': Command('ACMD41', 41, 3)
     }
 
     def __init__(self, spi=0, pin=17):
+        self._type = 'Unknown'
+        self._state = 'uninitialized'
         self._cs = Pin(pin, mode=Pin.OUT, value=1)
         self._spi = SPI(spi)
         self._spi.init(baudrate=400_000, polarity=0, phase=0, bits=8)
 
-    def execute(self, cmd, arg, txdata=b''):
+    def __str__(self) -> str:
+        return f'Card(_type: {self._type} _state: {self._state})'
+
+    def execute(self, cmd, arg=0, txdata=b''):
         """Run a command on the card and return a Response.""" 
 
         command = SDCard.commands[cmd]
@@ -83,7 +94,7 @@ class SDCard:
                 if b != b'\xff':
                     break
                 elif x >= 8:
-                    raise Error("No response within expected time frame")
+                    raise RuntimeError('No response within expected time frame')
 
             # Handle R3/R7 format responses
             if command.has_extra_response():
@@ -97,14 +108,62 @@ class SDCard:
     def go_idle_state(self):
         """Send software reset."""
 
-        return self.execute("CMD0", 0)
+        for x in range(3):
+            response = self.execute('CMD0')
+            if response.response == 1:
+                self._state = 'idle'
+                return response
+
+        # failed after 3 attempts...
+        raise RuntimeError('Software reset (CMD0) failed 3 attempts')
 
     def initialize(self):
-        """Reset card and """
-        pass
+        """Reset card and transition to the ready state."""
+
+        print('forcing card to native mode')
+        self.reset()
+
+        print('sending card to idle (CMD0)')
+        print(self.go_idle_state())
+
+        print('checking voltage (CMD8)')
+        response = self.send_if_cond()
+        print(response)
+
+        if response.is_illegal_command():
+            # Check for V1 card
+            pass
+        else:
+            # Handle V2 card
+            self._type = 'SDC V2'
+            if (response.extra != 0x1AA):
+                raise RuntimeError('Unusable card: voltage')
+            else:
+                # TODO should voltages be checked at this point?
+                print('reading OCR (CMD58)')
+                print(self.read_ocr())
+                print('initializing card (ACMD41)')
+                print(self.execute('CMD55'))
+                response = self.initialize_card()
+                print(response)
+                if response.response == 1:
+                    time.sleep_ms(500)
+                    print(self.execute('CMD55'))
+                    print(self.initialize_card())
+                    
+    def initialize_card(self):
+        """Initialize card (ACMD41)."""
+
+        return self.execute('ACMD41', 0x40000000)
+
+    def read_ocr(self):
+        """Read Operations Condition Register (OCR)."""
+
+        return self.execute('CMD58')
 
     def reset(self):
         """Force card to enter native operating mode by sending 80 clocks with DO high."""
+
         try:
             for x in range(10):
                 self._spi.write(b'\xff')
@@ -113,4 +172,4 @@ class SDCard:
 
     def send_if_cond(self):
 
-        return self.execute("CMD8", 0x1AA)
+        return self.execute('CMD8', 0x1AA)
--- py-sdcard/main.py
+++ py-sdcard/main.py
@@ -6,24 +6,12 @@ time.sleep(1) # settle time
 
 print("create SPI objects")
 card = SDCard(0)
+print(card)
 print(card._spi)
 
-print("forcing card to native mode")
-card.reset()
+card.initialize()
 
-print("sending card to idle (CMD0)")
-print(card.go_idle_state())
-
-print("checking voltage (CMD8)")
-print(card.send_if_cond())
-
-#r1 = Response(0x01)
-#print(r1)
-
-#r2 = Response(0x01, 0x12345678)
-#print(r2)
-
-print("done")
+print(card)
 #while True:
 #    time.sleep(1)
 
